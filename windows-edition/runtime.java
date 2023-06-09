419eb782f5ee5eda276599ed156f364c73efa30c
Cameron Scuderi <camscud@gmail.com>
2023-06-08 19:46:57 -0500

    an update, prob nothing works but I expect that

--- windows-edition/runtime.java
+++ windows-edition/runtime.java
@@ -11,42 +11,123 @@ import java.io.IOException;
 
 //random import
 import java.util.Random;
-public static int globalInterval;
+
+//file reading imports
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+//file writing imports
+import java.io.IOException;
+import java.io.FileWriter;
+//file making imports
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
     public static void main(String[] args) {
-        interval = helper.getInterval(); 
+//        interval = helper.getInterval(); 
         Timer screenshotTimer = new Timer();
          
-        TimerTask task = new TimerTask()  {     
-        
+        TimerTask task = new myTimer(); 
          
-        timer.schedule(task, 200, interval);  
-    }
+        //run the program at the smallest interval, and only run what needs to at that time.
+        screenshotTimer.schedule(task, 10000, 2000);//every 2 seconds run the program  
+   } 
 }
-public class myTimer extends TimerTask {
-    public void run(){
 
+public class myTimer extends TimerTask {
+    private boolean takeAndAnalyzeScreenshot(String timeFileName)
+    {
+    
         Instant thisInstant = new Instant();
         System.out.println(thisInstant.captureScreen());
         boolean currentScreenshotContainsPorn = thisInstant.checkIsPornRandom("temp-screenshot.png");
         
         if(currentScreenshotContainsPorn)
         {
-        //do the action phase
-        action.actionDisplayCondensendingText();
-        //increase the interval that the checker would run after the action is completed
+            //do the action phase
+            timeSinceIncident = 0;
+            fileHandler.writeToFile(timeFileName, timeSinceIncident);
+            action.actionDisplayCondensendingText();
+            return true;
         } 
+
+
+
         else
         {
-        System.out.println("nothing bad was found");
+            timeSinceIncident+=2;
+            fileHandler.writeToFile(timeFileName, timeSinceIncident);
+            System.out.println("nothing bad was found");
+            return false;
         }
+        return false;
     }
- 
+
+    public void run(){
+        String timeFileName = "/newtxt.txt";
+
+        int timeSinceIncident = fileHandler.makeAndReadFile(timeFileName);  
+        //interval shortly after incident
+        //interval slowly after incident (that day)
+        //default interval and a day after incident
+        //long term "sleep" interval for if no activity was found for a while
+        
+        int sleepinterval3 = 660480003;         // 300s (5 min) interval until next violation
+
+        int defaultInterval2 = 604800000;         // 120s (2 min) for 24-168 hours after interval2
+
+        int intermediateInterval1 = 86400000;          // 30s for 0-24 hours after interval1
+        int intervalFollowingIncident0 = 1800000;          // 10s for 0-30min after violation
+         
+
+        int intervalState = 0;
+
+        if(timeSinceIncident < intervalFollowingIncident0){//interval 0
+        intervalState = 0; 
+        }
+        else if(timeSinceIncident < intermediateInterval1){//interval 1
+        intervalState = 1;
+        }
+        else if(timeSinceIncident < defaultInterval2){//interval 2
+        intervalState = 2
+        }
+        else if(timeSinceIncident < sleepInterval3){// sleep interval
+        intervalState = 3
+        }
 
 
+
+
+//I know I can move them up there but I already have moved them down here so here we go.
+
+        if(intervalState==0){
+         
+        } 
+        else if(intervalState==1){
+        
+        }
+        else if(intervalState==2){
+
+        }
+        else if(intervalState==3){
+
+        }
+
+
+        if(timeSinceIncident==sleepInterval) 
+        {
+            if(
+            takeAndAnalyzeScreenshot(timeFileName);   
+             
+
+}
 
 
 }
+
+
 
 //SCREENSHOT**********************SCREENSHOT*************SCREENSHOT**********
 
@@ -109,7 +190,89 @@ public class action{
     
 
 }
-public class helper{
+public class fileHandler{
+    public static String readFile(String theFileName){
+        String data = "0";
+        try 
+        {
+            File myObj = new File(theFileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) 
+            {
+                data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+            return data;
+        }       
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred. in reading the file.");
+            e.printStackTrace();
+        }
+        return "failed to read file";
+    }
+    public static void writeToFile(String theFileName, int data) {
+        try {
+            FileWriter myWriter = new FileWriter(theFileName);
+            myWriter.write(data);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } 
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static String makeAndReadFile(String theFileName) {
+        try {
+            File myObj = new File(theFileName);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+                return "-1"; //denote that the file was created and needs writing to
+            } 
+            else { //file already exists - run this code
+                System.out.println("File already exists. passing already existing value");
+                return readFile(theFileName); 
+            }
+        } 
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return "error 404";
+    }
+}
+/*
+    public static void writeToFile(String theFilename, int data) throws IOException {
+        final File file = new File(theFilename);
+
+        try(final DataOutputStream dos = new DataOutputStream(new FileOutputStream(file))) {
+            dos.writeInt(data);
+            System.out.println(data+" was successfully added to the file");
+        }
+        //catch(){
+         //   System.out.println("there was an error in editing the file");
+        //}
+ 
+    }
+    public static int readFile(String theFilename){
+        final File file = new File(theFilename);
+        int readData;
+        System.out.println("ran \"readfile\" script");
+        try (final DataInputStream dis = new DataInputStream(new FileInputStream(file))) {
+            readData = dis.readInt();
+            System.out.println("Reading int from file: " + readData);
+            //globalInterval = currentInterval;
+        }
+
+        catch(){ //this runs when there is not already a file made and sets it to the first boot interval
+            System.out.println("the file that contains the interval has an error");
+        }
+        return readData;
+*/
+
+
+/*
     public static int getInterval() throws IOException {
         final File file = new File("/interval.txt");
 
@@ -139,5 +302,6 @@ public class helper{
             System.out.println("there was an error in editing the file");
         }
     }
+*/
 
-}
+
